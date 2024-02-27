c80d1f59b02a626def6330264483818039a46f4f
Vivek <vivekmunde@users.noreply.github.com>
2024-02-27 15:09:01 +0530

    Users collection

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,55 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { admin, quotes } from "./seed-data";
 const db = new PrismaClient();
 
 async function seed() {
+  const adminUser = await db.users.create({
+    data: admin,
+  });
+
   await Promise.all(
-    getQuotes().map((quote) => {
-      return db.quotes.create({ data: quote });
+    quotes.map((quote) => {
+      return db.quotes.create({
+        data: { ...quote, createdBy: adminUser.id },
+      });
     })
   );
 }
 
 seed();
-
-function getQuotes() {
-  return [
-    {
-      title: "The only thing we have to fear is fear itself.",
-      author: "Franklin D. Roosevelt",
-    },
-    {
-      title:
-        "If you can’t communicate, it’s like winking at a girl in the dark – nothing happens.",
-      author: "Warren Buffett",
-    },
-    {
-      title:
-        "Do not save what is left after spending, but spend what is left after saving.",
-      author: "Warren Buffett",
-    },
-    {
-      title: "Risk comes from not knowing what you’re doing.",
-      author: "Warren Buffett",
-    },
-    {
-      title:
-        "It takes 20 years to build a reputation and five minutes to ruin it.",
-      author: "Warren Buffett",
-    },
-    {
-      title:
-        "Someone’s sitting in the shade today because someone planted a tree a long time ago.",
-      author: "Warren Buffett",
-    },
-    {
-      title:
-        "Read 500 pages like this every day. That’s how knowledge works. It builds up, like compound interest.",
-      author: "Warren Buffett",
-    },
-    {
-      title:
-        "It’s better to hang out with people better than you. Pick out associates whose behavior is better than yours, and you’ll drift in that direction.",
-      author: "Warren Buffett",
-    },
-  ];
-}
