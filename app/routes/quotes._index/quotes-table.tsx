f7d5f113accd90b669f3fc44c918085cc3afe604
Vivek <vivekmunde@users.noreply.github.com>
2024-02-29 15:16:07 +0530

    Show quote and author in single cell

--- app/routes/quotes._index/quotes-table.tsx
+++ app/routes/quotes._index/quotes-table.tsx
@@ -1,13 +1,6 @@
 import { Link } from "@remix-run/react";
 import React from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "~/components/ui/table";
 
 const QuotesTable: React.FC<{
   quotes: { id: string; title: string; author: string | null }[];
@@ -15,20 +8,17 @@ const QuotesTable: React.FC<{
   return (
     <div className="border rounded">
       <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Title</TableHead>
-            <TableHead>Author</TableHead>
-          </TableRow>
-        </TableHeader>
         <TableBody>
           {quotes.map((quote) => (
             <TableRow key={quote.id}>
               <TableCell className="align-top">
-                <Link to={`/quotes/${quote.id}`}>{quote.title}</Link>
-              </TableCell>
-              <TableCell className="align-top text-nowrap">
-                {quote.author ?? ""}
+                <Link to={`/quotes/${quote.id}`} className="block">
+                  <span>{quote.title}</span>
+                  <span className="text-neutral-500">
+                    {" - "}
+                    {quote.author ?? "Unknown"}
+                  </span>
+                </Link>
               </TableCell>
             </TableRow>
           ))}
