da13931b1d51a02a184885f3d0332c7f650c4edb
Vivek <vivekmunde@users.noreply.github.com>
2024-03-05 19:08:17 +0530

    Move redirect outside of defer

--- app/routes/auth.login/route-loader.server.ts
+++ app/routes/auth.login/route-loader.server.ts
@@ -1,22 +1,12 @@
-import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import getRandomQuote from "~/api/get-random-quote.server";
-import { isLoggedIn } from "~/utils/server/auth";
 import { TData } from "./types";
 
 const getData = async (): Promise<TData> => {
   return getRandomQuote({ select: { title: true, author: true } });
 };
 
-const loader = async (args: LoaderFunctionArgs) => {
-  const _isLoggedIn = await isLoggedIn(args.request);
-  if (_isLoggedIn) {
-    const { searchParams } = new URL(args.request.url);
-    const redirectTo = searchParams.get("redirectTo");
-
-    return redirect(redirectTo ?? "/quotes");
-  } else {
-    return await getData();
-  }
+const loader = async () => {
+  return await getData();
 };
 
 export default loader;
--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -1,6 +1,8 @@
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import withDecideRouteType from "~/components/route/with-decide-route-type";
 import withRouteAction from "~/components/route/with-route-action";
 import { decideLoaderType } from "~/utils/route";
+import { isLoggedIn } from "~/utils/server/auth";
 import routeAction from "./route-action.server";
 import RouteDefault, { loader as defaultLoader } from "./route-default";
 import RouteDeferred, { loader as deferredLoader } from "./route-deferred";
@@ -8,7 +10,17 @@ import RouteError from "./route-error";
 
 export const ErrorBoundary = RouteError;
 
-export const loader = decideLoaderType(defaultLoader, deferredLoader);
+export const loader = async (args: LoaderFunctionArgs) => {
+  const _isLoggedIn = await isLoggedIn(args.request);
+  if (_isLoggedIn) {
+    const { searchParams } = new URL(args.request.url);
+    const redirectTo = searchParams.get("redirectTo") ?? "";
+
+    return redirect(redirectTo.length > 0 ? redirectTo : "/quotes");
+  } else {
+    return decideLoaderType(defaultLoader, deferredLoader)(args);
+  }
+};
 
 export const action = routeAction;
 
