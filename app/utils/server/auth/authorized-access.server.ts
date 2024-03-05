3c7a48d8a3060c8aed8aec416ec4f45bb6b77201
Vivek <vivekmunde@users.noreply.github.com>
2024-03-05 18:28:19 +0530

    Return from logout

--- app/utils/server/auth/authorized-access.server.ts
+++ app/utils/server/auth/authorized-access.server.ts
@@ -1,25 +1,25 @@
+import { TypedResponse } from "@remix-run/node";
 import logout from "~/utils/server/auth/logout.server";
 import { db } from "../db.server";
-import getUserSession from "./get-user-session.server";
+import getUserId from "./get-user-id.server";
 
 export default async function authorizedAccess<T>(
   request: Request,
   fn: () => T
-): Promise<T> {
-  const session = await getUserSession(request);
-  const userId = session.get("userId");
+): Promise<T | TypedResponse<never>> {
+  const userId = await getUserId(request);
 
-  if (typeof userId !== "string" || !userId) {
-    logout(request);
+  if (typeof userId !== "string" || (userId ?? "").length === 0) {
+    return await logout(request);
   }
 
-  const user = await db.quotes.findUnique({
+  const user = await db.users.findUnique({
     where: { id: userId },
     select: { id: true },
   });
 
   if (!user?.id) {
-    logout(request);
+    return await logout(request);
   }
 
   return fn();
