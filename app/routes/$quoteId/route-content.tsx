0e42a4e4fc56543cff2c7eca0df053f7e4569684
Vivek <vivekmunde@users.noreply.github.com>
2024-03-08 20:55:45 +0530

    Remove search route

--- app/routes/$quoteId/route-content.tsx
+++ app/routes/$quoteId/route-content.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from "@remix-run/react";
 import Layout from "~/components/layout";
-import Header from "./components/header";
 
 const RouteContent: React.FC = () => {
   return (
     <Layout.Screen>
-      <Header />
+      <Layout.Screen.Header />
       <Outlet />
     </Layout.Screen>
   );
--- app/routes/$quoteId/route-error.tsx
+++ app/routes/$quoteId/route-error.tsx
@@ -1,11 +1,10 @@
 import Layout from "~/components/layout";
 import CommonRouteError from "~/components/route-error";
-import Header from "./components/header";
 
 const RouteContent: React.FC = () => {
   return (
     <Layout.Screen>
-      <Header />
+      <Layout.Screen.Header />
       <CommonRouteError />
     </Layout.Screen>
   );
