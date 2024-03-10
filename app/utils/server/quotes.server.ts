08d3d052dc7c187a11525d183251a7059df680ea
Vivek <vivekmunde@users.noreply.github.com>
2024-03-10 21:23:27 +0530

    Remove blanks

--- app/utils/server/quotes.server.ts
+++ app/utils/server/quotes.server.ts
@@ -1,15 +1,18 @@
+const urlSegmentSeparator = "___";
+
 export const getQuoteUrlSegment = (id: string, title: string) => {
   return [
     title
       .replace(/[^a-z0-9 ]/gi, "")
       .split(" ")
+      .filter((it) => it.trim().length > 0)
       .slice(0, 10)
       .map((it) => it.toLowerCase())
       .join("-"),
     id,
-  ].join("___");
+  ].join(urlSegmentSeparator);
 };
 
 export const getQuoteIdFrmomUrlSegment = (urlSegment: string) => {
-  return urlSegment.split("___").pop() ?? urlSegment;
+  return urlSegment.split(urlSegmentSeparator).pop() ?? urlSegment;
 };
