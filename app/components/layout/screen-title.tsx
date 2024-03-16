807f3135b267351cd4c86a727e90cf77d254b7d1
Vivek <vivekmunde@users.noreply.github.com>
2024-03-16 13:55:45 +0530

    Screen title style

--- app/components/layout/screen-title.tsx
+++ app/components/layout/screen-title.tsx
@@ -1,3 +1,20 @@
-import ScreenTitle from "~/components/ui/typography/h2";
+import React from "react";
+import H2 from "~/components/ui/typography/h2";
+import layoutStyles from "~/styles/layout";
+
+const ScreenTitle: React.FC<{
+  className?: string;
+  children: React.ReactNode;
+}> = ({ className, children }) => {
+  return (
+    <H2
+      className={[layoutStyles.screen.title.base, className]
+        .filter(Boolean)
+        .join(" ")}
+    >
+      {children}
+    </H2>
+  );
+};
 
 export default ScreenTitle;
--- app/routes/quotes._index/route-content.tsx
+++ app/routes/quotes._index/route-content.tsx
@@ -38,7 +38,7 @@ const RouteContent: React.FC<{
               <Layout.Screen.Title className="flex-1">
                 Quotes
               </Layout.Screen.Title>
-              <div className="flex flex-row items-start gap-2 sm:mb-4 md:w-[60%]">
+              <div className="flex flex-row items-start gap-2 sm:mb-4 md:mb-0 md:w-[60%]">
                 <div className="flex-1">
                   <SearchForm />
                 </div>
